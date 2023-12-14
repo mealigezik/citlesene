28fbe691692998eb631e71bec9148e47bfbde61d
Ali Gezik <bilgepanter@gmail.com>
2023-12-14 12:37:48 +0100

    q

---
+++ citlesene-2/script.js
@@ -0,0 +1,36 @@
+let sepet = [];
+
+function sepeteEkle(cesit) {
+    const urun = {
+        cesit: cesit,
+        kilo: 5, // Örnek olarak her ürün 5 kg
+        fiyat: cesit === 'tuzlu' ? 85 : 75 // Tuzlu cekirdek 10 TL/kg, tuzsuz cekirdek 12 TL/kg
+    };
+
+    sepet.push(urun);
+    guncelleSepet();
+}
+
+function guncelleSepet() {
+    const sepetListesi = document.getElementById('sepetListesi');
+    const toplamTutarElement = document.getElementById('toplamTutar');
+
+    sepetListesi.innerHTML = '';
+    let toplamTutar = 0;
+
+    sepet.forEach(urun => {
+        const li = document.createElement('li');
+        li.textContent = `${urun.cesit} cekirdek - ${urun.kilo} kg - ${urun.fiyat * urun.kilo} TL`;
+        sepetListesi.appendChild(li);
+
+        toplamTutar += urun.fiyat * urun.kilo;
+    });
+
+    toplamTutarElement.textContent = `Toplam Tutar: ${toplamTutar} TL`;
+}
+
+function siparisiTamamla() {
+    alert('Siparişiniz alındı! Teşekkür ederiz.');
+    sepet = [];
+    guncelleSepet();
+}
